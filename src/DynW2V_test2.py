0046bfa2f2299e4de130927f8453e10d64ca7c24
Chengbin Hou <chengbin.hou10@foxmail.com>
2019-01-16 23:38:04 +0000

    DynW2V_test2 V3

--- src/DynW2V_test2.py
+++ src/DynW2V_test2.py
@@ -1,22 +1,36 @@
 '''
-Dynamic Word2Vec example
+Dynamic Word2Vec example to address the following questions
 by Chengbin Hou
-ref: https://rutumulkar.com/blog/2015/word2vec and https://radimrehurek.com/gensim/models/word2vec.html
+
+# basic useage of Gemsim Word2Vec: given sentences -> train embedding [done]
+
+# how to update for unseen words [done]
+# how to continue traning on seen + unseen words [done]
+# check if updated embedding and similarity are changed? [yes]
+# how Gensim Word2Vec update embedding [done]
+
+# how to reset input embedding matrix [done]
+# can I take out output embedding matrix? and reset? [yes]
+
+# how to update embedding using one pair --> train_sg_pair [to do...]
+# check if more freq a pair occurs, more closer they are [yes]
+# check if and only if the center node are updated, but its positive sample and negative samples are remained [to do...]
+OR try to directly use sentences? walks? but how to obtain good walks???
 '''
 
 import gensim
 import logging
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-sentences = [['a', 'b', 'c'], ['d', 'e', 'f'],['a', 'b', 'c']]
+sentences = [['c','a','c'],['c','a','b'],['c','b','d']]
 
 w2v = gensim.models.Word2Vec(size=6, window=1, sg=1, hs=0, negative=1, ns_exponent=1.0,
-                            alpha=0.025, min_alpha=0.0001, min_count=1, sample=0.001, iter=5, workers=4, seed=2019,
+                            alpha=0.025, min_alpha=0.0001, min_count=1, sample=0.001, iter=20, workers=4, seed=2019,
                             sentences=None, corpus_file=None, sorted_vocab=1, batch_words=10000, compute_loss=False,
                             max_vocab_size=None, max_final_vocab=None, trim_rule=None)
 
-w2v.build_vocab(sentences=sentences, update=False) #try True for the first time?
-w2v.train(sentences=sentences,total_examples=w2v.corpus_count, epochs=w2v.iter) #change iter???
+w2v.build_vocab(sentences=sentences, update=False)
+w2v.train(sentences=sentences,total_examples=w2v.corpus_count, epochs=w2v.iter) #change iter??? #reset input embedding matrix to 0
 
 print('\n -----------------------------------------------')
 print('wv for a ', w2v.wv['a'])
@@ -29,20 +43,20 @@ del w2v
 w2v = pickle.loads(myw2v)
 '''
 
-w2v.wv.vectors[0] = [1,2,3,4,5,6]
-sentences = [['a', 'd', 'f'],['a', 'd', 'f'],['a', 'd', 'f'],['a', 'd', 'f'],['a', 'd', 'f'],['a', 'd', 'f'],['a', 'd', 'f']]
-w2v.build_vocab(sentences=sentences, update=True)
+# w2v.wv.vectors[0] = [1,2,3,4,5,6] #reset input embedding
+# w2v.syn1neg[0] = [7,8,9,10,11,12] #reset output embedding
+sentences = [['a','e'],['a','d']]
+w2v.build_vocab(sentences=sentences, update=True) #Copy all the existing weights, and reset the weights to zeros for the newly added vocabulary
 w2v.train(sentences=sentences,total_examples=w2v.corpus_count, epochs=w2v.iter)
 print('\n -----------------------------------------------')
-print('wv for a ', w2v.wv['a'])
+print('wv for a ', w2v.wv['a']) # b, c, d
 print('similar_by_word for a', w2v.wv.similar_by_word('a'))
-# 试试相似度变化大不大
-# 怎么自己来重置某一个向量；或者不充值继续训练？
-# 看看他的update那个到底做了什么
 
 
-sentences = [['d', 'a', 'f'],['d', 'a', 'f'],['d', 'a', 'f']]
-w2v.build_vocab(sentences=sentences, update=True)
+
+
+sentences = [['a','kk'],['a','b','a','b','a','b','a']]
+w2v.build_vocab(sentences=sentences, update=True) #
 w2v.train(sentences=sentences,total_examples=w2v.corpus_count, epochs=w2v.iter)
 print('\n -----------------------------------------------')
 print('wv for a ', w2v.wv['a'])
