08a8f9c9ca5779c67edf2f2cd963b6be8a8a4008
Chengbin Hou <chengbin.hou10@foxmail.com>
2019-01-18 20:19:22 +0000

    add more comments

--- synthetic_LFR.py
+++ synthetic_LFR.py
@@ -19,12 +19,12 @@ nx.draw_networkx(G)
 plt.show(G)
 """
 
-def generate_initial_LFR(n=100):
-    tau1 = 3
-    tau2 = 1.5
-    mu = 0.1
-    G = nx.algorithms.community.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=5, min_community=10,seed=4)
+def generate_initial_LFR(n=100, tau1=3, tau2=1.5, mu=0.1, average_degree=4, min_community=10, max_community=None, seed=0):
+    # https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.community.community_generators.LFR_benchmark_graph.html#networkx.algorithms.community.community_generators.LFR_benchmark_graph
+    G = nx.algorithms.community.LFR_benchmark_graph(n=n, tau1=tau1, tau2=tau2, mu=mu,
+                                average_degree=average_degree, min_community=min_community, max_community=max_community, seed=seed)
     G.remove_edges_from(G.selfloop_edges())
+    print('The initial LFR graph is nx.is_directed(G)?', nx.is_directed(G))
 
     degree_dict = dict(G.degree()) # {node ID: degree, ...}
     # print('degree_dict', degree_dict)
@@ -37,16 +37,16 @@ def generate_initial_LFR(n=100):
         community = list(communities[i])
         for j in range(len(community)): # j goes over all nodes
             community_dict[community[j]] = i
-    # print('community_dict', community_dict) # {node ID: community ID, ...}
+    print('community_dict', community_dict) # {node ID: community ID, ...}
 
     return G, community_dict, degree_dict
 
-def generate_dynamic_data(initial_G, community_dict, degrees,time_step=3, initial_edge_porpotation=0.7):
-    reference_graph = initial_G.copy()
-    initial_nodes_number = len(degrees)
+def generate_dynamic_data(initial_G, community_dict, time_step=4, initial_edge_porpotation=0.6): #why this name initial_edge_porpotation  
+    # reference_graph = initial_G.copy()
+    initial_nodes_number = len(initial_G.nodes())
     initial_edges_number = len(initial_G.edges())
     initial_edges = list(initial_G.edges())
-    #disappear_nodes_number = int(initial_edges_number*initial_edge_porpotation)
+    # disappear_nodes_number = int(initial_edges_number*initial_edge_porpotation)
     chance_each_time_step = (1-initial_edge_porpotation)/time_step
 
     time_step_slots = []
@@ -75,17 +75,15 @@ def generate_dynamic_data(initial_G, community_dict, degrees,time_step=3, initia
     for i in range(len(graphs)):
         nx.draw_networkx(graphs[i])
         if i > 0:
+            # print('nx.is_directed(G)?', nx.is_directed(graphs[i]))
             print("edge deleted",set(graphs[i-1].edges()) - set(graphs[i].edges()))
             print("edge added",set(graphs[i].edges()) - set(graphs[i-1].edges()))
             print("node deleted",set(graphs[i - 1].nodes()) - set(graphs[i].nodes()))
             print("node added",set(graphs[i].nodes()) - set(graphs[i - 1].nodes()))
-            # 为了解决这个问题，这里需要再看看LRF生成的图是否应该有方向。如果没有方向，直接把每个graph变为undirect就行了
-            # https://networkx.github.io/documentation/stable/reference/functions.html#graph
-            # G = nx.to_undirected(G)
-            # 另外，在增加减少边时候，还要注意一个edge (a,b) 和 (b,a)的问题，可能需要一个小函数来处理
-
-            # 如果是有方向的，我们还需要进一步考虑：1）我们只针对无向图做吗？2）如果也做有向图，会遇到什么大麻烦吗？
-            # 我个人想法，我还还是focus在无方向图，具体原因我们在讨论。
+            # 我们这次文章只做undirected graph；
+            # 1）直接用networkx的undirected graph，目前就是
+            # 2）用networkx的directed graph，但是我们所有的处理都必须是double edge比如只要有(a,b)，必须加或者减（a,b）+(b,a)
+            # G_directed = nx.to_directed(G_undirected)
 
         print("graph_size: ", len(graphs[i]), '====== @ time step', i)
 
@@ -121,5 +119,5 @@ if __name__ == '__main__':
     save_any_obj(obj=community_dict, path='LFR_community_dict.data')  # {node ID: degree, ...}
     save_any_obj(obj=degree_dict, path='LFR_degree_dict.data')  # {node ID: community ID, ...}
 
-    Gs = generate_dynamic_data(G, community_dict, degree_dict)
+    Gs = generate_dynamic_data(G, community_dict)
     save_nx_graph(nx_graph=Gs, path='LFR_dynamic_graphs.data')
