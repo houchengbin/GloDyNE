feefc7de3c4a43defa0cfcb2aca66b6b7f3ffe98
Chengbin Hou <chengbin.hou10@foxmail.com>
2019-01-18 13:32:46 +0000

    add pickle dump to disk

--- synthetic_LFR.py
+++ synthetic_LFR.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import pickle
 
 """
 n = 250
@@ -18,26 +19,29 @@ nx.draw_networkx(G)
 plt.show(G)
 """
 
-def generate_initial_LFR(n=50):
+def generate_initial_LFR(n=100):
     tau1 = 3
     tau2 = 1.5
     mu = 0.1
     G = nx.algorithms.community.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=5, min_community=10,seed=4)
     G.remove_edges_from(G.selfloop_edges())
+
+    degree_dict = dict(G.degree()) # {node ID: degree, ...}
+    print('degree_dict', degree_dict)
+
+
     communities = {frozenset(G.nodes[v]['community']) for v in G}
-    degrees = G.degree()
-    print(G,degrees)
     community_dict = {}
     communities = list(communities)
-    for i in range(len(communities)):
+    for i in range(len(communities)): # i is community ID
         community = list(communities[i])
-        for j in range(len(community)):
-            community_dict[str(community[j])] = i
-            #print(community[j],"  ",i)
-            #print(community_dict[str(community[j])])
-    return G,community_dict,degrees
+        for j in range(len(community)): # j goes over all nodes
+            community_dict[community[j]] = i
+    print('community_dict', community_dict) # {node ID: community ID, ...}
 
-def generate_dynamic_data(initial_G,community_dict,degrees,time_step = 10,initial_edge_porpotation = 0.7):
+    return G, community_dict, degree_dict
+
+def generate_dynamic_data(initial_G, community_dict, degrees,time_step=3, initial_edge_porpotation=0.7):
     reference_graph = initial_G.copy()
     initial_nodes_number = len(degrees)
     initial_edges_number = len(initial_G.edges())
@@ -67,6 +71,8 @@ def generate_dynamic_data(initial_G,community_dict,degrees,time_step = 10,initia
         for j in range(current_edge_time_step, len(graphs)):
             graphs[j].add_edge(list(initial_edges[i])[0],list(initial_edges[i])[1])
 
+    save_dynamic_graphs(graphs)
+
     for i in range(len(graphs)):
         nx.draw_networkx(graphs[i])
         if i > 0:
@@ -81,9 +87,24 @@ def generate_dynamic_data(initial_G,community_dict,degrees,time_step = 10,initia
 
 
 
+def save_dynamic_graphs(nx_graphs, path='dynamic_graphs.data'):
+    with open(path, 'wb') as f:
+        pickle.dump(nx_graphs, f, protocol=pickle.HIGHEST_PROTOCOL) #the higher protocol, the smaller file
+
+    with open(path, 'rb') as f:
+        nx_graphs_reload = pickle.load(f)
+    for i in range(len(nx_graphs)):
+        print('Check if it is correctly dumped and loaded: ', nx_graphs_reload[i].edges() == nx_graphs[i].edges(), 'for Graph ', i)
+
+def save_any_obj(obj, path='obj_temp.data'):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
+# ----------------- test start from here -----------------------------
+G, community_dict, degree_dict = generate_initial_LFR()
+save_any_obj(obj=community_dict, path='community_dict.data')
+save_any_obj(obj=degree_dict, path='degree_dict.data')
 
-g1,g2,g3 = generate_initial_LFR()
-generate_dynamic_data(g1,g2,g3)
+generate_dynamic_data(G, community_dict, degree_dict)
