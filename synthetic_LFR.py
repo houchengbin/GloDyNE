a8042015f3e0c6529be3d4f61bd73c8f2c042783
briskhan <aynhzhanghan@gmail.com>
2019-01-19 14:03:00 +0000

    graph compare, node_ID change to str

--- synthetic_LFR.py
+++ synthetic_LFR.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import pickle
 
 """
 n = 250
@@ -10,7 +11,6 @@ tau2 = 1.5
 mu = 0.1
 G = nx.algorithms.community.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=7, min_community=30)
 communities = {frozenset(G.nodes[v]['community']) for v in G}
-
 print(communities)
 print(list(list(list(communities))[0]))
 print(G.degree())
@@ -18,38 +18,48 @@ nx.draw_networkx(G)
 plt.show(G)
 """
 
-def generate_initial_LFR(n=50):
-    tau1 = 3
-    tau2 = 1.5
-    mu = 0.1
-    G = nx.algorithms.community.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=5, min_community=10,seed=4)
+
+def generate_initial_LFR(n=100, tau1=3, tau2=1.5, mu=0.1, average_degree=4, min_community=10, max_community=None,
+                         seed=0):
+    # https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.community.community_generators.LFR_benchmark_graph.html#networkx.algorithms.community.community_generators.LFR_benchmark_graph
+    G = nx.algorithms.community.LFR_benchmark_graph(n=n, tau1=tau1, tau2=tau2, mu=mu,
+                                                    average_degree=average_degree, min_community=min_community,
+                                                    max_community=max_community, seed=seed)
     G.remove_edges_from(G.selfloop_edges())
+    print('The initial LFR graph is nx.is_directed(G)?', nx.is_directed(G))
+
+    degree_dict = dict(G.degree())  # {node ID: degree, ...}
+    # print('degree_dict', degree_dict)
+
+
     communities = {frozenset(G.nodes[v]['community']) for v in G}
-    degrees = G.degree()
-    print(G,degrees)
     community_dict = {}
     communities = list(communities)
-    for i in range(len(communities)):
+    for i in range(len(communities)):  # i is community ID
         community = list(communities[i])
-        for j in range(len(community)):
+        for j in range(len(community)):  # j goes over all nodes
             community_dict[str(community[j])] = i
-            #print(community[j],"  ",i)
-            #print(community_dict[str(community[j])])
-    return G,community_dict,degrees
+    print('community_dict', community_dict)  # {node ID: community ID, ...}
 
-def generate_dynamic_data(initial_G,community_dict,degrees,time_step = 10,initial_edge_porpotation = 0.7):
-    reference_graph = initial_G.copy()
-    initial_nodes_number = len(degrees)
+    return G, community_dict, degree_dict
+
+
+
+
+
+def generate_dynamic_data(initial_G, community_dict, time_step=4,
+                          initial_edge_porpotation=0.6):  # why this name initial_edge_porpotation
+    # reference_graph = initial_G.copy()
+    initial_nodes_number = len(initial_G.nodes())
     initial_edges_number = len(initial_G.edges())
     initial_edges = list(initial_G.edges())
-    #disappear_nodes_number = int(initial_edges_number*initial_edge_porpotation)
-    chance_each_time_step = (1-initial_edge_porpotation)/time_step
+    # disappear_nodes_number = int(initial_edges_number*initial_edge_porpotation)
+    chance_each_time_step = (1 - initial_edge_porpotation) / time_step
 
     time_step_slots = []
     time_step_slots.append(initial_edge_porpotation)
     for i in range(time_step):
-        time_step_slots.append(initial_edge_porpotation + (i+1)*chance_each_time_step)
-
+        time_step_slots.append(initial_edge_porpotation + (i + 1) * chance_each_time_step)
 
     edges_time_step = []
     for i in range(initial_edges_number):
@@ -60,30 +70,77 @@ def generate_dynamic_data(initial_G,community_dict,degrees,time_step = 10,initia
                 break
 
     graphs = []
-    for i in range(time_step+1):
+    for i in range(time_step + 1):
         graphs.append(nx.Graph())
     for i in range(len(edges_time_step)):
         current_edge_time_step = edges_time_step[i]
         for j in range(current_edge_time_step, len(graphs)):
-            graphs[j].add_edge(list(initial_edges[i])[0],list(initial_edges[i])[1])
+            graphs[j].add_edge(str(list(initial_edges[i])[0]), str(list(initial_edges[i])[1]))
 
     for i in range(len(graphs)):
         nx.draw_networkx(graphs[i])
         if i > 0:
-            print("edge decrease",set(graphs[i-1].edges())-set(graphs[i].edges()))
-            print("edge increase",set(graphs[i].edges()) - set(graphs[i-1].edges()))
-            print("node decrease",set(graphs[i - 1].nodes()) - set(graphs[i].nodes()))
-            print("node increase",set(graphs[i].nodes()) - set(graphs[i - 1].nodes()))
+            # print('nx.is_directed(G)?', nx.is_directed(graphs[i]))
+            print("edge deleted v1", set(graphs[i - 1].edges()) - set(graphs[i].edges()))
+            print("edge deleted v2", edge_s1_minus_s0(graphs[i-1],graphs[i]))
+            print("edge added v1", set(graphs[i].edges()) - set(graphs[i - 1].edges()))
+            print("edge added v2", edge_s1_minus_s0(graphs[i],graphs[i-1]))
+            print("node deleted", set(graphs[i].nodes()) - set(graphs[i-1].nodes()))
+            print("node added", set(graphs[i].nodes()) - set(graphs[i - 1].nodes()))
+            # 我们这次文章只做undirected graph；
+            # 1）直接用networkx的undirected graph，目前就是
+            # 2）用networkx的directed graph，但是我们所有的处理都必须是double edge比如只要有(a,b)，必须加或者减（a,b）+(b,a)
+            # G_directed = nx.to_directed(G_undirected)
 
-        print("graph_size:",len(graphs[i]))
+        print("graph_size: ", len(graphs[i]), '====== @ time step', i)
 
         plt.show(graphs[i])
 
+    return graphs
+
+def edge_s1_minus_s0(s1, s0, is_directed=False):
+    if not is_directed:
+        s1_reordered = set((a, b) if int(a) < int(b) else (b, a) for a, b in s1.edges())
+        s0_reordered = set((a, b) if int(a) < int(b) else (b, a) for a, b in s0.edges())
+        return s1_reordered - s0_reordered
+    else:
+        print('currently not support directed case')
 
 
+def unique_nodes_from_edge_set(edge_set):
+    unique_nodes = []
+    for a, b in edge_set:
+        if a not in unique_nodes:
+            unique_nodes.append(a)
+        if b not in unique_nodes:
+            unique_nodes.append(b)
+    return unique_nodes
+
+def save_nx_graph(nx_graph, path='nx_graph_temp.data'):
+    with open(path, 'wb') as f:
+        pickle.dump(nx_graph, f, protocol=pickle.HIGHEST_PROTOCOL)  # the higher protocol, the smaller file
+    with open(path, 'rb') as f:
+        nx_graph_reload = pickle.load(f)
+
+    try:
+        print('Check if it is correctly dumped and loaded: ', nx_graph_reload.edges() == nx_graph.edges(),
+              ' It contains only ONE graph')
+    except:
+        for i in range(len(nx_graph)):
+            print('Check if it is correctly dumped and loaded: ', nx_graph_reload[i].edges() == nx_graph[i].edges(),
+                  ' for Graph ', i)
 
 
+def save_any_obj(obj, path='obj_temp.data'):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-g1,g2,g3 = generate_initial_LFR()
-generate_dynamic_data(g1,g2,g3)
+if __name__ == '__main__':
+    G, community_dict, degree_dict = generate_initial_LFR()
+    save_nx_graph(nx_graph=G, path='LFR_static_graph.data')
+    save_any_obj(obj=community_dict, path='LFR_community_dict.data')  # {node ID: degree, ...}
+    save_any_obj(obj=degree_dict, path='LFR_degree_dict.data')  # {node ID: community ID, ...}
+
+    Gs = generate_dynamic_data(G, community_dict)
+    save_nx_graph(nx_graph=Gs, path='LFR_dynamic_graphs.data')
